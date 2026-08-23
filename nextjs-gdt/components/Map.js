3e7de0d21a34e19d7f152abdad79ec87f6b00624
Joshua Galvan <joshuakael.galvan7@yahoo.com>
2026-08-23 17:34:30 +0100

    added initial useState logic for events

--- nextjs-gdt/components/Map.js
+++ nextjs-gdt/components/Map.js
@@ -1,12 +1,22 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css"; 
 import icon from "@/components/icon";
 
-export default function Home() {
+export default function Map() {
   const position = [51.505, -0.09]
-        
+
+  const eventsState = useState([]);
+  const events = eventsState[0];
+  const setEvents = eventsState[1];
+
+
+  async function getEvents() {
+
+  }
+
 return (
   <MapContainer center={position} zoom={4} scrollWheelZoom={true} style={{
     height: "100vh",
